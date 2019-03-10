834a6dadd9823cfe47a71cc12c493d8f1ec3dd0d
unknown <l4wl3ss1989@gmail.com>
2019-03-10 19:50:58 +0100

    basic layaout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxilar from '../../hoc/Auxilar';
 import styles from './Layout.module.scss';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxilar>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Auxilar>
-    
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerColseHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (
+            <Auxilar>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerColseHandler}
+                />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Auxilar>
+        )
+    }
+}
+
+export default Layout;
