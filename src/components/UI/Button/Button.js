d610d684c52a516742ccde8e4755b2ec5c5b71b8
unknown <l4wl3ss1989@gmail.com>
2019-03-20 22:49:11 +0100

    added form construction and basic validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,6 +4,7 @@ import styles from './Button.module.scss';
 
 const button = (props) => (
     <button className={[styles.Button, styles[props.btnType]].join(' ')}
+        disabled={props.disabled}
         onClick={props.clicked}
     >
         {props.children}
